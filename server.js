1f8aea52e43c0b890da3853238d5eb81b9f953b5
Rohan <rohanasif1990@gmail.com>
2022-10-17 03:31:12 +0500

    add get and post requests

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors())
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.get('/', function sendData(req, res) {
+    res.send(projectData);
+})
+
+app.post('/', function receiveData(req, res) {
+    res.send(projectData);
+})
 
 // Setup Server
+app.listen(3000, () => {
+    console.log("Weather Journal App listening on port 3000")
+    console.log("Go to http://localhost:3000")
+})
+
