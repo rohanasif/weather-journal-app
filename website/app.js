0916b791ccb6d4f1f969f2ac9dac4797984e3d5c
Rohan <rohanasif1990@gmail.com>
2022-10-17 04:08:42 +0500

    Add api key and baseurl

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '495c16bf9a0ac87f9a9f64850b6a6cb3';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
