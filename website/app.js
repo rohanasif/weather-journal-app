ce060ad89c189c23fd621a61c52c0b80358a2ffb
Rohan <rohanasif1990@gmail.com>
2022-10-17 17:08:01 +0500

    some more changes

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 /* Global Variables */
 const weatherURL = "https://api.openweathermap.org/data/3.0/onecall";
 const zipURL = "http://api.openweathermap.org/geo/1.0/zip"
-const key = "495c16bf9a0ac87f9a9f64850b6a6cb3";
+const key = "125f42aff073c9c5f0887b46a4dca02d";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -10,35 +10,6 @@ let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 // Use zipURL to receive lon and lat by using zipcode
 const userInfo = document.getElementById("userInfo");
 const generate = document.getElementById("generate");
-
-// Function to get the coords using the zipcode entered in the form
-const getCoords = async (zipURL, zip, key) => {
-
-    // res equals to the result of fetch function
-    const res = await fetch(`${zipURL}?zip=${zip}&appid=${key}`);
-    try {
-
-        // data equals to the result of fetch function
-        const coords = await res.json();
-        return coords;
-    }
-    catch (error) {
-        console.log("error", error);
-    }
-};
-
-// Function to get the weather data using the lat and lon
-const getWeatherData = async (weatherURL, coords, key) => {
-    const res = await fetch(`${weatherURL}?lat=${coords.lat}&lon=${coords.lon}&appid=${key}`);
-    try {
-        const data = await res.json();
-
-        return data;
-    }
-    catch (error) {
-        console.log("error", error);
-    }
-}
 
 generate.addEventListener("click", (e) => {
     e.preventDefault();
@@ -68,6 +39,35 @@ generate.addEventListener("click", (e) => {
     }
 
 })
+
+// Function to get the coords using the zipcode entered in the form
+const getCoords = async (zipURL, zip, key) => {
+
+    // res equals to the result of fetch function
+    const res = await fetch(`${zipURL}?zip=${zip}&appid=${key}`);
+    try {
+
+        // data equals to the result of fetch function
+        const coords = await res.json();
+        return coords;
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+};
+
+// Function to get the weather data using the lat and lon
+const getWeatherData = async (weatherURL, coords, key) => {
+    const res = await fetch(`${weatherURL}?lat=${coords.lat}&lon=${coords.lon}&appid=${key}`);
+    try {
+        const data = await res.json();
+
+        return data;
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+}
 
 /* Function to POST data */
 const postData = async (url = "", data = {}) => {
